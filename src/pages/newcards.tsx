d3debfc08eea76e110efd897f55a2e74fd578b54
Sandon Lai <sandonnlai@gmail.com>
2022-09-16 13:19:47 +1000

    added form elements

--- src/pages/newcards.tsx
+++ src/pages/newcards.tsx
@@ -1,24 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
+import { trpc } from "../utils/trpc";
 
 interface NewCardsProps {}
 
 const NewCards = ({}: NewCardsProps) => {
+  const [formData, setFormData] = useState({
+    front: "",
+    back: "",
+  });
+  const newCard = trpc.useMutation(["card.createCard"]);
+
+  const divStyling = "flex flex-col text-purple-300 text-3xl font-bold gap-2";
+
+  //   const handleNewCard = async () => {
+  //       await newCard.mutateAsync({
+  //           front:
+  //       })
+  //   }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <>
       <Header />
       <div className="h-screen w-screen flex flex-col justify-center items-center">
-        <div className="flex flex-col p-4 gap-4">
-          <div className="flex flex-col text-purple-300 text-3xl font-bold">
-            Front
-            <input className="rounded-md p-3 text-black" />
-          </div>
-          <div className="flex flex-col text-purple-300 text-3xl">
-            Back
-            <input />
-          </div>
-          <button> Create Card </button>
+        <div className="text-purple-200 text-lg font-bold">
+          Front - Target Sentence <br />
+          Back - Target Word and it's definition
         </div>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col p-4 gap-4 w-96">
+            <div className={divStyling}>
+              Front
+              <input
+                value={formData.front}
+                onChange={handleChange}
+                name="front"
+                className="rounded-md p-3 text-black text-lg"
+              />
+            </div>
+            <div className={divStyling}>
+              Back
+              <input
+                value={formData.back}
+                onChange={handleChange}
+                name="back"
+                className="rounded-md p-3 text-black text-lg"
+              />
+            </div>
+            <button className="px-6 py-4 m-3 rounded-md bg-purple-200 text-2xl text-center font-bold hover:bg-purple-100 duration-300 w-56 self-center">
+              Create Card
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
